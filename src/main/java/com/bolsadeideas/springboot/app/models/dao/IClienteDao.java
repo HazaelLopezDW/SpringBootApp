3910f9fcf3309ddf63dd003bd2ee4a3f960d62fa
Hazael Lopez <elhazael134@gmail.com>
2023-10-07 11:52:51 -0600

    Bonus track: Paginador desde cero - Usando PagingAndSortingRepository - Parte I

--- src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
@@ -1,11 +1,11 @@
 package com.bolsadeideas.springboot.app.models.dao;
 
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long>{
+public interface IClienteDao extends JpaRepository<Cliente, Long>{
 	
 
 }
