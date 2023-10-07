13d939157b426d357c65d043c284b977f9fc6dc1
Hazael Lopez <elhazael134@gmail.com>
2023-10-06 22:30:04 -0600

    Agregando editar

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
@@ -15,6 +18,7 @@ import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import jakarta.validation.Valid;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
 	
 	@Autowired
@@ -40,8 +44,26 @@ public class ClienteController {
 		return "form";
 	}
 	
+	@RequestMapping(value="/form/{id}", method=RequestMethod.GET)
+	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+		
+		Cliente cliente = null;
+		
+		if(id > 0) {
+			cliente = clienteDao.findOne(id);
+		}else {
+			return "redirect:/listar";
+		}
+		
+		model.put("titulo", "Editar Cliente");
+		model.put("cliente", cliente);
+		
+		return "form";
+	}
+	
+	
 	@RequestMapping(value="/form", method=RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
 		
 		if(result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario De Clientes");
@@ -49,6 +71,7 @@ public class ClienteController {
 		}
 		
 		clienteDao.save(cliente);
+		status.setComplete();
 		return "redirect:/listar";
 	}
 	
