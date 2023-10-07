4bebf6e39e4e436f8fb9235d86ef2bb9acd97a28
Hazael Lopez <elhazael134@gmail.com>
2023-10-06 21:40:52 -0600

    Agregando campo fecha en formulario

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -3,6 +3,8 @@ package com.bolsadeideas.springboot.app.models.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,12 +29,8 @@ public class Cliente implements Serializable {
 	
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
-
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
 	
 	
 	public Long getId() {
