ee6519f3d63460b5bba2e29c9a4cda38bfa882d4
Hazael Lopez <elhazael134@gmail.com>
2023-10-07 23:10:37 -0600

    Agregamos un nuevo código que esta mejor

--- src/main/java/com/bolsadeideas/springboot/app/util/paginator/PageRender.java
+++ src/main/java/com/bolsadeideas/springboot/app/util/paginator/PageRender.java
@@ -1,9 +1,9 @@
 package com.bolsadeideas.springboot.app.util.paginator;
 
-import org.springframework.data.domain.Page;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.data.domain.Page;
 
 public class PageRender<T> {
 	
@@ -11,59 +11,54 @@ public class PageRender<T> {
 	private Page<T> page;
 	
 	private int totalPaginas;
-	private int numElementosPorPagina;
-	
+	private int numElementosPorPaginas;
 	private int paginaActual;
 	
 	private List<PageItem> paginas;
-	
 	
 	public PageRender(String url, Page<T> page) {
 		this.url = url;
 		this.page = page;
 		this.paginas = new ArrayList<PageItem>();
 		
-		numElementosPorPagina = page.getSize();
-		totalPaginas = page.getTotalPages();
-		paginaActual = page.getNumber() + 1;
+		this.numElementosPorPaginas = page.getSize();
+		this.totalPaginas = page.getTotalPages();
+		this.paginaActual = page.getNumber() + 1;
 		
 		int desde, hasta;
-		if(totalPaginas <= numElementosPorPagina) {
+		
+		if(this.totalPaginas <= this.numElementosPorPaginas) {
 			desde = 1;
-			hasta = totalPaginas;
+			hasta = this.totalPaginas;
 		} else {
-			if(paginaActual <= numElementosPorPagina/2) {
+			if(this.paginaActual <= this.numElementosPorPaginas/2) {
 				desde = 1;
-				hasta = numElementosPorPagina;
-			}else if(paginaActual >= totalPaginas - numElementosPorPagina/2) {
-				desde = totalPaginas - numElementosPorPagina + 1;
-				hasta = numElementosPorPagina;
+				hasta = this.numElementosPorPaginas;
+			} else if(this.paginaActual >= this.totalPaginas - this.numElementosPorPaginas/2) {
+				desde = this.totalPaginas - this.numElementosPorPaginas + 1;
+				hasta = this.numElementosPorPaginas;
 			} else {
-				desde = paginaActual - numElementosPorPagina/2;
-				hasta = numElementosPorPagina;
+				desde = this.paginaActual - this.numElementosPorPaginas/2;
+				hasta = this.numElementosPorPaginas;
 			}
 		}
 		
 		for(int i=0; i < hasta; i++) {
-			paginas.add(new PageItem(desde + 1, paginaActual == desde+1));
+			paginas.add(new PageItem(desde + i, this.paginaActual == desde+i));
 		}
 	}
-
 
 	public String getUrl() {
 		return url;
 	}
 
-
 	public int getTotalPaginas() {
 		return totalPaginas;
 	}
 
-
 	public int getPaginaActual() {
 		return paginaActual;
 	}
-
 
 	public List<PageItem> getPaginas() {
 		return paginas;
@@ -85,4 +80,5 @@ public class PageRender<T> {
 	public boolean isHasPrevious() {
 		return page.hasPrevious();
 	}
+
 }
