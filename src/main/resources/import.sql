da53f521eb5f5ba00abfe8de9029c5e57b9b9603
Hazael Lopez <elhazael134@gmail.com>
2023-10-09 10:49:56 -0600

    Analizando y revisando las tablas y relaciones en MySQL Workbench

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -40,3 +40,21 @@ INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('Barry', 
 INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('Adam', 'Warlock', 'warlock@vengadores.com', '2023-08-07', '');
 
 /* Populate tabla productos */
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic Pantalla LCD', 259990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Soni Camara Digital DSC-W3220B', 123490, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Apple Ipod shuffle', 1499990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony Notebook ZliD', 37900, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Newlett Packard Multifuncional F2280', 69990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Bianchi Bicicleta Aro 26', 69990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Mica comoda 5 cajones', 299990, NOW());
+
+/* Creamos algunas facturas */
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura equipos de oficina', null, 1, NOW());
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 1);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(2, 1, 4);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 5);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 7);
+
+/* Creamos algunas facturas */
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura Bicicleta', 'Alguna nota importante', 1, NOW());
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(3, 2, 5);
