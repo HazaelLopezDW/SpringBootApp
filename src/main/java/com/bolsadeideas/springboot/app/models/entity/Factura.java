2dd7b1f2ea502d3f1dccd409224e8a910a081e87
Hazael Lopez <elhazael134@gmail.com>
2023-10-09 10:49:18 -0600

    Asociaciones: ManyToOne Unidireccional - Clases Entity ItemFactura y Producto

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
@@ -19,6 +19,7 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="facturas")
@@ -94,6 +95,17 @@ public class Factura implements Serializable {
 
 	public void addItemFactura(ItemFactura item) {
 		this.items.add(item);
+	}
+	
+	public Double getTotal() {
+		Double total = 0.0;
+		
+		int size = items.size();
+		
+		for(int i=0; i < size; i++) {
+			total += items.get(i).calcularImporte();
+		}
+		return total;
 	}
 
 
--- src/main/java/com/bolsadeideas/springboot/app/models/entity/ItemFactura.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/ItemFactura.java
@@ -3,9 +3,12 @@ package com.bolsadeideas.springboot.app.models.entity;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,6 +20,10 @@ public class ItemFactura implements Serializable {
 	private Long id;
 
 	private Integer cantidad;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="producto_id")
+	private Producto producto;
 	
 	
 
@@ -36,14 +43,13 @@ public class ItemFactura implements Serializable {
 		this.cantidad = cantidad;
 	}
 	
-	public Long calcularImporte() {
-		
-		return cantidad.longValue();
+	public Double calcularImporte() {
+		return cantidad.doubleValue() * producto.getPrecio();
 	}
 
-	/**
-	 * 
-	 */
+	
+	
+	
 	private static final long serialVersionUID = 1L;
 
 }
