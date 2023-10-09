3702f222a1185ab1ff1bfaaa10dbcb596198d4c1
Hazael Lopez <elhazael134@gmail.com>
2023-10-08 21:18:16 -0600

    Eliminar archivo de imagen

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
@@ -43,6 +44,7 @@ import jakarta.validation.Valid;
 public class ClienteController {
 	
 	private final Logger logger =  LoggerFactory.getLogger(getClass());
+	private final static String UPLOADS_FOLDER = "uploads";
 	
 	@Autowired
 	private IClienteService clienteService;
@@ -51,7 +53,7 @@ public class ClienteController {
 	@GetMapping(value="/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
 		
-		Path pathFoto = Paths.get("uploads").resolve(filename).toAbsolutePath();
+		Path pathFoto = Paths.get(UPLOADS_FOLDER).resolve(filename).toAbsolutePath();
 		logger.info("PathFoto:" + pathFoto);
 		
 		Resource recurso = null;
@@ -151,8 +153,19 @@ public class ClienteController {
 		
 		if(!foto.isEmpty()) {
 			
+			if(cliente.getId() != null && cliente.getId() > 0
+					&& cliente.getFoto() != null && cliente.getFoto().length() > 0) {
+				
+				Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
+				File archivo = rootPath.toFile();
+				
+				if(archivo.exists() && archivo.canRead()) {
+					archivo.delete();
+				}
+			}
+			
 			String uniqueFilename = UUID.randomUUID().toString() + "_" +foto.getOriginalFilename();
-			Path rootPath = Paths.get("uploads").resolve(uniqueFilename);
+			Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(uniqueFilename);
 			
 			Path rootAbsolutePath = rootPath.toAbsolutePath();
 			logger.info("rootPath: " + rootPath);
@@ -185,8 +198,20 @@ public class ClienteController {
 	public String eliminar(@PathVariable(value="id") Long id, RedirectAttributes flash) {
 		
 		if(id > 0) {
+			Cliente cliente = clienteService.findOne(id);
+			
 			clienteService.delete(id);
 			flash.addFlashAttribute("success", "Cliente eliminado con éxito");
+			
+			Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
+			File archivo = rootPath.toFile();
+			
+			if(archivo.exists() && archivo.canRead()) {
+				if(archivo.delete()) {
+					flash.addFlashAttribute("info", "Foto " + cliente.getFoto() 
+					+ " Eliminada con éxito!");
+				}
+			}
 		}
 		
 		return "redirect:/listar";
