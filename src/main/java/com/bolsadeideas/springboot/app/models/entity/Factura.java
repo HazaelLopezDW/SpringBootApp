c4cf313a632e69351b6f1d3cac0fb3c753b6ef99
Hazael Lopez <elhazael134@gmail.com>
2023-10-09 11:29:50 -0600

    Listando las facturas en la vista de detalle del Cliente - ver.html

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
@@ -19,7 +19,6 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="facturas")
