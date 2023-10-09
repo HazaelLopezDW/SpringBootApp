6feb78f7858006520fb9b0a384940996ac3bb9f0
Hazael Lopez <elhazael134@gmail.com>
2023-10-09 10:12:07 -0600

    Asociaciones: ManyToOne Bidireccional - Clases Entity Factura y Cliente

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -1,15 +1,20 @@
 package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -41,6 +46,17 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 	
+	@OneToMany(mappedBy="cliente" ,fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	private List<Factura> facturas;
+	
+	
+	public Cliente() {
+		this.facturas = new ArrayList<Factura>();
+	}
+
+
+
+
 	private String foto;
 	
 	
@@ -90,6 +106,20 @@ public class Cliente implements Serializable {
 
 	public void setFoto(String foto) {
 		this.foto = foto;
+	}
+
+
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+	
+	
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
 	}
 
 
