054c0d82a7ecf4d9a59f3405a046777c464f3c1a
Hazael Lopez <elhazael134@gmail.com>
2023-10-09 12:12:23 -0600

    Creando la vista formulario de la factura

--- src/main/java/com/bolsadeideas/springboot/app/controllers/FacturaController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/FacturaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
@@ -15,6 +16,7 @@ import com.bolsadeideas.springboot.app.models.service.IClienteService;
 
 @Controller
 @RequestMapping("/factura")
+@SessionAttributes("factura")
 public class FacturaController {
 	
 	@Autowired
