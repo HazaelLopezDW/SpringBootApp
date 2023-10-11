c177b338aa8ce89d80597d0ff8d612358b293c98
Hazael Lopez <elhazael134@gmail.com>
2023-10-11 17:00:52 -0600

    Obteniendo el usuario autenticado en el controlador - Segunda Forma

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -89,6 +90,15 @@ public class ClienteController {
 					authentication.name()
 			));
 		}
+		
+		org.springframework.security.core.Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		if(auth != null) {
+			logger.info("Utilizando forma estatica 'SecurityContextHolder.getContext().getAuthentication();' Hola usuario autenticado, Tu username es: ".concat(
+					auth.getName()
+			));
+		}
+		
 
 		Pageable pageRequest = PageRequest.of(page, 5);
 
