95b78a64e0353c2cd5bd173bf7e928c3926f6d81
Hazael Lopez <elhazael134@gmail.com>
2023-10-15 15:02:01 -0600

    Configuración de nuestro sistema multilenguaje y el Locale

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -1,11 +1,17 @@
 package com.bolsadeideas.springboot.app;
 
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 
 @Configuration
@@ -19,5 +25,30 @@ public class MvcConfig implements WebMvcConfigurer {
 	public static BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	/** Estos dos metodos los usamos para cambiar de idioma  **/
+	@Bean
+	public LocaleResolver localResolver() {
+		SessionLocaleResolver localResolver = new SessionLocaleResolver();
+		localResolver.setDefaultLocale(new Locale("es", "ES"));
+		return localResolver;
+	}
+	
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	
+	
 	
 }
